1452aca142b36c7047943e8de222ea9eb4dc046e
Sushant Rathi <sushant.rathi@glean.com>
2022-08-08 23:20:38 +0530

    minor changes
    - registered app via steve.smith@salessavvy.net
    - installed it on salessavvy1.atlassian.net

--- jira/src/index.js
+++ jira/src/index.js
@@ -1,10 +1,10 @@
-import { fetch, storage, webTrigger } from '@forge/api';
+import { fetch } from '@forge/api';
 
 export async function eventHandler(event, context) {
 	console.log('Event Captured')
 	// Replace this with scio-apps event handler
 	// for backend perms: https://developer.atlassian.com/platform/forge/manifest-reference/permissions/#external-permissions
-	var targetURL = 'https://webhook.site/7ef76c62-d490-4f9b-927f-7bb19a34fa84'
+	var targetURL = 'https://webhook.site/c780948b-91d8-40ee-800b-3a7096831bdf'
 	console.log(`Target URL ${targetURL}`)
 	const response = await fetch(targetURL, {
 		method: 'POST', 
