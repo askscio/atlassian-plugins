de8ab94b27cb4dbf4e621c05206750f356cf6c20
Sushant Rathi <sushant.rathi@glean.com>
2022-08-25 14:43:46 +0530

    [jira cloud] fixed async()=>e.data could not be cloned error

--- jira/cloud/src/index.js
+++ jira/cloud/src/index.js
@@ -4,12 +4,10 @@ export async function eventHandler(event, context) {
 	console.log('Event Captured')
 	// for backend perms: https://developer.atlassian.com/platform/forge/manifest-reference/permissions/#external-permissions
 	var targetURL = 'https://apps-be.glean.com/datasources/jira/forge'
-	console.log(`Target URL ${targetURL}`)
-	const response = await fetch(targetURL, {
+	const status = await fetch(targetURL, {
 		method: 'POST', 
 		body: JSON.stringify(event),
 		headers: {'Content-Type': 'application/json'}
-	})
-	return response
-	
+	}).then(response => response.status)
+	return status
 }
