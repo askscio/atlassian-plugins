608c275b1895eb7a17f4599ee923280c73d56164
Bharath Bhushan <bharath.bhushan@glean.com>
2023-02-07 10:36:06 +0530

    Review comments 1

--- jira/server/scio_search/src/main/java/ScioSearchConfigRestPlugin/impl/ScioSearchIssueSecurityMembers.java
+++ jira/server/scio_search/src/main/java/ScioSearchConfigRestPlugin/impl/ScioSearchIssueSecurityMembers.java
@@ -69,25 +69,28 @@ public class ScioSearchIssueSecurityMembers {
     response.startAt = 0;
     response.isLast = true; // no response pagination
     response.values = new ArrayList();
-    if (issueSecurityLevels != null) {
-      for (IssueSecurityLevel isl : issueSecurityLevels) {
-        logger.debug(isl.getDescription() + ":" + isl.getName() + ":" + String.valueOf(isl.getId()) + ":" + String.valueOf(isl.getSchemeId()));
-        List<IssueSecurityLevelPermission> issueSecurityLevelPermissions = issueSecuritySchemeManager.getPermissionsBySecurityLevel(isl.getId());
-        if (issueSecurityLevels != null) {
-          for (IssueSecurityLevelPermission islp : issueSecurityLevelPermissions) {
-            logger.debug(islp.getId() + ":" + islp.getParameter() + ":" + islp.getSchemeId() + ":" + islp.getSecurityLevelId() + ":" + islp.getType());
-            JiraPermissionHolderInfo holder = new JiraPermissionHolderInfo();
-            holder.type = islp.getType();
-            holder.parameter = islp.getParameter();
-            IssueSecuritySchemeMemberInfo memberInfo = new IssueSecuritySchemeMemberInfo();
-            memberInfo.id = String.valueOf(islp.getId());
-            memberInfo.issueSecurityLevelId = String.valueOf(islp.getSecurityLevelId());
-            memberInfo.holder = holder;
-            response.values.add(memberInfo);
-            response.maxResults += 1;
-            response.total += 1;
-          }
-        }
+
+    if (issueSecurityLevels == null) {
+      return response;
+    }
+    for (IssueSecurityLevel isl : issueSecurityLevels) {
+      logger.debug("IssueSecurityLevel: " + isl.getDescription() + ":" + isl.getName() + ":" + String.valueOf(isl.getId()) + ":" + String.valueOf(isl.getSchemeId()));
+      List<IssueSecurityLevelPermission> issueSecurityLevelPermissions = issueSecuritySchemeManager.getPermissionsBySecurityLevel(isl.getId());
+      if (issueSecurityLevelPermissions == null) {
+        continue;
+      }
+      for (IssueSecurityLevelPermission islp : issueSecurityLevelPermissions) {
+        logger.debug("IssueSecurityLevelPermission: " + islp.getId() + ":" + islp.getParameter() + ":" + islp.getSchemeId() + ":" + islp.getSecurityLevelId() + ":" + islp.getType());
+        JiraPermissionHolderInfo holder = new JiraPermissionHolderInfo();
+        holder.type = islp.getType();
+        holder.parameter = islp.getParameter();
+        IssueSecuritySchemeMemberInfo memberInfo = new IssueSecuritySchemeMemberInfo();
+        memberInfo.id = String.valueOf(islp.getId());
+        memberInfo.issueSecurityLevelId = String.valueOf(islp.getSecurityLevelId());
+        memberInfo.holder = holder;
+        response.values.add(memberInfo);
+        response.maxResults += 1;
+        response.total += 1;
       }
     }
     return response;
