ebee3775cbbec53aae2b6c204cf277b61852b5d4
Bharath Bhushan <108652741+bharath-bhushan-glean@users.noreply.github.com>
2023-04-25 14:54:56 +0530

    [JIRA SERVER PLUGIN]: Add instance info to /info endpoint (#23)

--- jira/server/scio_search/src/main/java/ScioSearchConfigRestPlugin/impl/ScioSearchInfoResponse.java
+++ jira/server/scio_search/src/main/java/ScioSearchConfigRestPlugin/impl/ScioSearchInfoResponse.java
@@ -6,6 +6,7 @@ import org.codehaus.jackson.annotate.JsonAutoDetect.Visibility;
 @JsonAutoDetect(fieldVisibility = Visibility.ANY)
 public class ScioSearchInfoResponse {
   public UserInfo userInfo;
+  public InstanceInfo instanceInfo;
 
   @JsonAutoDetect(fieldVisibility = Visibility.ANY)
   public static class UserInfo {
@@ -14,5 +15,11 @@ public class ScioSearchInfoResponse {
     public String fullName;
     public String email;
     public boolean isAdmin;
+  }
+
+  @JsonAutoDetect(fieldVisibility = Visibility.ANY)
+  public static class InstanceInfo {
+    String version;
+    String baseUrl;
   }
 }
