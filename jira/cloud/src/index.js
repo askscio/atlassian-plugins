c2528ab0d87366d23fbbd155a2c9ae7a933998f7
Adhiraj Somani <adhiraj.somani@glean.com>
2024-05-13 16:42:25 +0530

    [Jira][NI][2/3] Send installed events containing a healthcheck URL

--- jira/cloud/src/index.js
+++ jira/cloud/src/index.js
@@ -1,13 +1,49 @@
-import { fetch } from '@forge/api';
+import api, { fetch, route, webTrigger } from '@forge/api';
+
+async function doPost(payload, queryParams) {
+  // for backend perms: https://developer.atlassian.com/platform/forge/manifest-reference/permissions/#external-permissions
+  var targetURL = 'https://apps-be.glean.com/datasources/jira/forge'
+  if (!!queryParams) {
+    targetURL += '?' + queryParams
+  }
+  return fetch(targetURL, {
+    method: 'POST',
+    body: JSON.stringify(payload),
+    headers: {'Content-Type': 'application/json'}
+  })
+}
 
 export async function eventHandler(event, context) {
-	console.log('Event Captured')
-	// for backend perms: https://developer.atlassian.com/platform/forge/manifest-reference/permissions/#external-permissions
-	var targetURL = 'https://apps-be.glean.com/datasources/jira/forge'
-	const status = await fetch(targetURL, {
-		method: 'POST', 
-		body: JSON.stringify(event),
-		headers: {'Content-Type': 'application/json'}
-	}).then(response => response.status)
-	return status
+  console.log('Event Captured')
+  const response = await doPost(event, 'event_type=product');
+  return response.status;
+}
+
+export async function healthcheckHandler(request, context) {
+  console.log('Received healthcheck request')
+  return {
+    "statusCode": 200  // required
+  }
+}
+
+export async function installedHandler(event, context) {
+  console.log('App installed. Installation ID:', event.id);
+
+  const response = await api.asUser().requestJira(route`/rest/api/2/serverInfo`, {
+    headers: {
+      'Accept': 'application/json'
+    }
+  });
+  const data = await response.json();
+  const baseUrl = data.baseUrl
+
+  const healthcheckUrl = await webTrigger.getUrl("healthcheck-trigger");
+
+  const postResponse = await doPost({
+    'baseUrl': data.baseUrl,
+    'installationId': event.id,
+    'healthcheckUrl': healthcheckUrl,
+  }, 'event_type=installed')
+
+  return postResponse.status
 }
