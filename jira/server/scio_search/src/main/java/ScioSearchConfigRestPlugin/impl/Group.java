0468c2b522d2e29ee39addde2236554a51dc07fb
Rupinder Goyal <rupinder.goyal@glean.com>
2024-11-15 02:26:17 +0530

    [Jira Plugin] Added endpoint for project Permission scheme

---
+++ jira/server/scio_search/src/main/java/ScioSearchConfigRestPlugin/impl/Group.java
@@ -0,0 +1,8 @@
+package ScioSearchConfigRestPlugin.impl;
+
+import org.codehaus.jackson.annotate.JsonAutoDetect;
+
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class Group {
+    public String name;
+}
