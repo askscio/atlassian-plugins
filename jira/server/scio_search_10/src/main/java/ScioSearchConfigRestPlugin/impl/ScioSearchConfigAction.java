a9c1b9c4937f258a370c1ec8644e9f59f9b09626
Soham Samaddar <soham.samaddar@glean.com>
2025-03-24 20:53:56 +0530

    Update version

--- jira/server/scio_search_10/src/main/java/ScioSearchConfigRestPlugin/impl/ScioSearchConfigAction.java
+++ jira/server/scio_search_10/src/main/java/ScioSearchConfigRestPlugin/impl/ScioSearchConfigAction.java
@@ -16,56 +16,58 @@ import static ScioSearchConfigRestPlugin.impl.MyPluginComponentImpl.TARGET_CONFI
 
 @SupportedMethods({RequestMethod.POST})
 public class ScioSearchConfigAction extends JiraWebActionSupport {
-  @JiraImport private PluginSettingsFactory pluginSettingsFactory;
-  private String target;
+    @JiraImport
+    private PluginSettingsFactory pluginSettingsFactory;
+    private String target;
 
-  public ScioSearchConfigAction() {}
-
-  @Inject
-  public ScioSearchConfigAction(PluginSettingsFactory pluginSettingsFactory) {
-    this.pluginSettingsFactory = pluginSettingsFactory;
-  }
-
-  public String getTarget() {
-    return target;
-  }
-
-  public void setTarget(String target) {
-    this.target = target;
-  }
-
-  public void setPluginSettingsFactory(PluginSettingsFactory pluginSettingsFactory) {
-    this.pluginSettingsFactory = pluginSettingsFactory;
-  }
-
-  @Override
-  @SupportedMethods({RequestMethod.POST})
-  public void doValidation() {
-    String targetUrl = getTarget();
-    if (targetUrl == null || targetUrl.isEmpty()) {
-      addErrorMessage("Target URL is required");
-      return;
+    public ScioSearchConfigAction() {
     }
-    try {
-      new URL(targetUrl);
-    } catch (MalformedURLException e) {
-      addErrorMessage("Invalid target URL");
+
+    @Inject
+    public ScioSearchConfigAction(PluginSettingsFactory pluginSettingsFactory) {
+        this.pluginSettingsFactory = pluginSettingsFactory;
     }
-  }
 
-  @Override
-  @SupportedMethods({RequestMethod.GET})
-  public String doDefault() throws Exception {
-    PluginSettings pluginSettings = pluginSettingsFactory.createGlobalSettings();
-    setTarget((String) pluginSettings.get(TARGET_CONFIG_KEY));
-    return super.doDefault();
-  }
+    public String getTarget() {
+        return target;
+    }
 
-  @Override
-  @SupportedMethods({RequestMethod.POST})
-  public String doExecute() throws Exception {
-    PluginSettings pluginSettings = pluginSettingsFactory.createGlobalSettings();
-    pluginSettings.put(TARGET_CONFIG_KEY, getTarget());
-    return SUCCESS;
-  }
+    public void setTarget(String target) {
+        this.target = target;
+    }
+
+    public void setPluginSettingsFactory(PluginSettingsFactory pluginSettingsFactory) {
+        this.pluginSettingsFactory = pluginSettingsFactory;
+    }
+
+    @Override
+    @SupportedMethods({RequestMethod.POST})
+    public void doValidation() {
+        String targetUrl = getTarget();
+        if (targetUrl == null || targetUrl.isEmpty()) {
+            addErrorMessage("Target URL is required");
+            return;
+        }
+        try {
+            new URL(targetUrl);
+        } catch (MalformedURLException e) {
+            addErrorMessage("Invalid target URL");
+        }
+    }
+
+    @Override
+    @SupportedMethods({RequestMethod.GET})
+    public String doDefault() throws Exception {
+        PluginSettings pluginSettings = pluginSettingsFactory.createGlobalSettings();
+        setTarget((String) pluginSettings.get(TARGET_CONFIG_KEY));
+        return super.doDefault();
+    }
+
+    @Override
+    @SupportedMethods({RequestMethod.POST})
+    public String doExecute() throws Exception {
+        PluginSettings pluginSettings = pluginSettingsFactory.createGlobalSettings();
+        pluginSettings.put(TARGET_CONFIG_KEY, getTarget());
+        return SUCCESS;
+    }
 }
--- jira/server/scio_search_10/src/main/java/ScioSearchConfigRestPlugin/impl/ScioSearchServiceAccountAction.java
+++ jira/server/scio_search_10/src/main/java/ScioSearchConfigRestPlugin/impl/ScioSearchServiceAccountAction.java
@@ -8,6 +8,7 @@ import com.atlassian.sal.api.pluginsettings.PluginSettings;
 import com.atlassian.sal.api.pluginsettings.PluginSettingsFactory;
 
 import javax.inject.Inject;
+
 import static ScioSearchConfigRestPlugin.impl.MyPluginComponentImpl.SERVICE_ACCOUNT_USER_EMAIL_CONFIG_KEY;
 
 // This class is used to configure service account user email in the plugin Settings from the Jira
@@ -15,60 +16,62 @@ import static ScioSearchConfigRestPlugin.impl.MyPluginComponentImpl.SERVICE_ACCO
 // It does a simple validation check before saving the email in the plugin settings.
 @SupportedMethods({RequestMethod.POST})
 public class ScioSearchServiceAccountAction extends JiraWebActionSupport {
-  @JiraImport private PluginSettingsFactory pluginSettingsFactory;
-  private String serviceAccountUserEmail;
+    @JiraImport
+    private PluginSettingsFactory pluginSettingsFactory;
+    private String serviceAccountUserEmail;
 
-  public ScioSearchServiceAccountAction() {}
-
-  @Inject
-  public ScioSearchServiceAccountAction(PluginSettingsFactory pluginSettingsFactory) {
-    this.pluginSettingsFactory = pluginSettingsFactory;
-  }
-
-  public String getServiceAccountUserEmail() {
-    return serviceAccountUserEmail;
-  }
-
-  public void setServiceAccountUserEmail(String email) {
-    this.serviceAccountUserEmail = email;
-  }
-
-  public void setPluginSettingsFactory(PluginSettingsFactory pluginSettingsFactory) {
-    this.pluginSettingsFactory = pluginSettingsFactory;
-  }
-
-  @Override
-  @SupportedMethods({RequestMethod.POST})
-  public void doValidation() {
-    String email = getServiceAccountUserEmail();
-    if (email == null || email.trim().isEmpty()) {
-      addErrorMessage("Email is required");
-      return;
+    public ScioSearchServiceAccountAction() {
     }
-    email = email.trim();
-    /**
-     * Must have text before and after '@' Must have a dot ('.') in the domain with text on both
-     * sides
-     */
-    String emailRegex = "^[^@]+@[^@]+\\.[^@]+$";
-    if (!email.matches(emailRegex)) {
-      addErrorMessage("Invalid email format");
+
+    @Inject
+    public ScioSearchServiceAccountAction(PluginSettingsFactory pluginSettingsFactory) {
+        this.pluginSettingsFactory = pluginSettingsFactory;
     }
-  }
 
-  @Override
-  @SupportedMethods({RequestMethod.GET})
-  public String doDefault() throws Exception {
-    PluginSettings pluginSettings = pluginSettingsFactory.createGlobalSettings();
-    setServiceAccountUserEmail((String) pluginSettings.get(SERVICE_ACCOUNT_USER_EMAIL_CONFIG_KEY));
-    return super.doDefault();
-  }
+    public String getServiceAccountUserEmail() {
+        return serviceAccountUserEmail;
+    }
 
-  @Override
-  @SupportedMethods({RequestMethod.POST})
-  public String doExecute() throws Exception {
-    PluginSettings pluginSettings = pluginSettingsFactory.createGlobalSettings();
-    pluginSettings.put(SERVICE_ACCOUNT_USER_EMAIL_CONFIG_KEY, getServiceAccountUserEmail());
-    return SUCCESS;
-  }
+    public void setServiceAccountUserEmail(String email) {
+        this.serviceAccountUserEmail = email;
+    }
+
+    public void setPluginSettingsFactory(PluginSettingsFactory pluginSettingsFactory) {
+        this.pluginSettingsFactory = pluginSettingsFactory;
+    }
+
+    @Override
+    @SupportedMethods({RequestMethod.POST})
+    public void doValidation() {
+        String email = getServiceAccountUserEmail();
+        if (email == null || email.trim().isEmpty()) {
+            addErrorMessage("Email is required");
+            return;
+        }
+        email = email.trim();
+        /**
+         * Must have text before and after '@' Must have a dot ('.') in the domain with text on both
+         * sides
+         */
+        String emailRegex = "^[^@]+@[^@]+\\.[^@]+$";
+        if (!email.matches(emailRegex)) {
+            addErrorMessage("Invalid email format");
+        }
+    }
+
+    @Override
+    @SupportedMethods({RequestMethod.GET})
+    public String doDefault() throws Exception {
+        PluginSettings pluginSettings = pluginSettingsFactory.createGlobalSettings();
+        setServiceAccountUserEmail((String) pluginSettings.get(SERVICE_ACCOUNT_USER_EMAIL_CONFIG_KEY));
+        return super.doDefault();
+    }
+
+    @Override
+    @SupportedMethods({RequestMethod.POST})
+    public String doExecute() throws Exception {
+        PluginSettings pluginSettings = pluginSettingsFactory.createGlobalSettings();
+        pluginSettings.put(SERVICE_ACCOUNT_USER_EMAIL_CONFIG_KEY, getServiceAccountUserEmail());
+        return SUCCESS;
+    }
 }
